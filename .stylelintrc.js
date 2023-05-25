551fff4d2227ac20971302e3f179a894bb2338d5
Mario Yepes <marioy47@gmail.com>
2023-05-25 16:14:25 -0500

    Using @wordpress/scripts for linting, formatting and styling

---
+++ .stylelintrc.js
@@ -0,0 +1,1 @@
+module.exports = require( '@wordpress/stylelint-config/scss' );
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,30 @@
-const path = require('path');
+const webpackWpConfig = require( '@wordpress/scripts/config/webpack.config' );
+const BrowserSyncPlugin = require( 'browser-sync-webpack-plugin' );
+const FixStyleOnlyEntriesPlugin = require( 'webpack-fix-style-only-entries' );
+const path = require( 'path' );
 
 module.exports = {
+	...webpackWpConfig,
 	entry: {
-		frontend: './src/js/frontend.js',
+		'js/scripts': './js/scripts.js',
+		'css/styles': './scss/styles.scss',
 	},
 	output: {
+		// Change the output folder from build to dist
+		path: path.resolve( __dirname, 'dist' ),
 		filename: '[name].js',
-		path: path.resolve(__dirname, 'js'),
 	},
-	devtool: process.env.NODE_ENV == 'production' ? false : 'eval-source-map',
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/preset-env', '@babel/preset-react'],
-				},
-			},
-		],
-	},
-	mode: process.env.NODE_ENV == 'production' ? 'production' : 'development',
+	plugins: [
+		...webpackWpConfig.plugins,
+
+		// Remove the empty JS files that webpack creates after compiling SCSS files
+		new FixStyleOnlyEntriesPlugin(),
+
+		// Add browsersync for automatic browser reload
+		new BrowserSyncPlugin( {
+			host: 'localhost',
+			port: 3000,
+			proxy: process.env.PROXY_URL ?? 'https://wordpress-plugin-starter.lndo.site/',
+		} ),
+	],
 };
