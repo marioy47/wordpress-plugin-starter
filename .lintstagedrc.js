5405f57a7eaa4a7f0137fff7148ecad6723e74e1
Mario Yepes <marioy47@gmail.com>
2023-05-26 09:01:34 -0500

    Adding husky and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'*.scss': 'npm run format:css',
+	'*.js': 'npm run format:js',
+	'*.md': 'npm run format:md',
+	'*.php': 'composer format',
+};
---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+// If your plugin add JavaScript, use this file as a
+// STARTING file for all scripts. You can split the scripts
+// into multiple files and include them using `import` or `require`
